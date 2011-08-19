136578ad0ec8a7aae68a70209004ab1ba30b87dd
gatk-lilly <ngslilly@yahoo.com>
2011-08-19 15:06:42 -0400

    Don't check if the directories are present before we create them (we're now blowing away the directory before we start, so why check for things guaranteed not to exist?)

--- public/shell/process_one_paired_end_lane.sh
+++ public/shell/process_one_paired_end_lane.sh
@@ -1,67 +1,64 @@
 #!/bin/bash
 
-#$ -N GATKLaneTest
+#$ -N LaneProcessingPipeline
 #$ -S /bin/bash
 #$ -cwd
 #$ -j y
 #$ -m be
 
+export ID=$1
+export SM=$2
+export LB=$3
+export PU=$4
+export PL=$5
+export CN=$6
+export DT=$7
+export FLOWCELL=$8
+export LANE=$9
+export FQ1=${10}
+export FQ2=${11}
+
 export JAVA_HOME=$HOME/opt/jdk1.6.0_27/
 export PATH=$HOME/opt/apache-ant-1.8.2/bootstrap/bin:$HOME/opt/apache-ant-1.8.2/dist/bin:$HOME/opt/jdk1.6.0_27/bin:$HOME/opt/jdk1.6.0_27/db/bin:$HOME/opt/jdk1.6.0_27/jre/bin:$HOME/opt/git-1.7.6/bin:$HOME/opt/git-1.7.6/perl/blib/bin:$HOME/opt/bwa-0.5.9:$HOME/opt/samtools-0.1.17:$PATH
 
-export WORK=/mnt/scratch
+export WORK=/mnt/scratch/$SM/$ID
 export RESOURCES=$WORK/resources
 export TMP=$WORK/tmp
 export DATA=$WORK/data
+export S3_BUCKET="ngs-kiran"
 
 export GATK="java -Djava.io.tmpdir=$TMP -jar $HOME/opt/GATK-Lilly/dist/GenomeAnalysisTK.jar";
 export QUEUE="java -Djava.io.tmpdir=$TMP -jar $HOME/opt/GATK-Lilly/dist/Queue.jar";
 export CPUS=`cat /proc/cpuinfo | grep -c processor`
 
+export BAM_BASE="$FLOWCELL.$LANE"
+export BAM=$BAM_BASE.bam
+export BAI=$BAM_BASE.bai
+
+echo "Creating $WORK directory"
+rm -rf $WORK
+mkdir -p $WORK
+mkdir $DATA
+mkdir $TMP
+
+echo "Changing to $WORK directory"
 cd $WORK
 
-if [ ! -e $TMP ]; then
-	mkdir $TMP
-fi
+echo "Extracting NGS resources"
+s3cmd sync s3://$S3_BUCKET/resources $WORK/
+gunzip -c $RESOURCES/resources.tar.gz | tar -C $RESOURCES -xf -
 
-if [ ! -e $WORK/resources/resources.tar.gz ]; then
-	s3cmd sync s3://$S3_BUCKET/resources $WORK/
-fi
+echo "Downloading fastqs"
+s3cmd sync $FQ1 $DATA/end1.fq.gz
+s3cmd sync $FQ2 $DATA/end2.fq.gz
 
-if [ ! -e $RESOURCES/ucsc.hg19.fasta ]; then
-	gunzip -c $RESOURCES/resources.tar.gz | tar -C $RESOURCES -xf -
-fi
+echo "Running lane-level pipeline"
+$QUEUE -S $HOME/opt/GATK-Lilly/public/scala/qscript/org/broadinstitute/sting/queue/qscripts/LaneProcessingPipeline.scala -bwa $HOME/opt/bwa-0.5.9/bwa -R $RESOURCES/ucsc.hg19.fasta -f1 $DATA/end1.fq.gz -f2 $DATA/end2.fq.gz -name $BAM -rg '@RG\tID:$ID\tSM:$SM\tLB:$LB\tPU:$PU\tPL:$PL\tCN:$CN\tDT:$DT' -threads $CPUS
 
-if [ ! -e $DATA ]; then
-	mkdir $DATA
-fi
+echo "Uploading results"
+S3_UPLOAD_PATH="s3://$S3_BUCKET/lanes/ACRG/$SM/$ID/"
 
-if [ ! -e $DATA/end1.fq.gz ]; then
-	s3cmd sync $1 $DATA/end1.fq.gz
-fi
-
-if [ ! -e $DATA/end2.fq.gz ]; then
-	s3cmd sync $2 $DATA/end2.fq.gz
-fi
-
-if [ ! -e $3 ]; then
-	$QUEUE -S $HOME/opt/GATK-Lilly/public/scala/qscript/org/broadinstitute/sting/queue/qscripts/LaneProcessingPipeline.scala -bwa $HOME/opt/bwa-0.5.9/bwa -R $RESOURCES/ucsc.hg19.fasta -f1 $DATA/end1.fq.gz -f2 $DATA/end2.fq.gz -name $3 -rg $4 -threads $CPUS $5
-fi
-
-LANE_DIR=`echo $3 | sed 's/.bam//'`
-
-BAM=$3
-S3_UPLOAD_BAM="$S3_BUCKET/lanes/ACRG/$LANE_DIR/";
-
-BAI=`echo $3 | sed 's/.bam/.bai/'`
-S3_UPLOAD_BAI=`echo $S3_UPLOAD_BAM | sed 's/.bam/.bai/g'`;
-
-if [ ! -e bam.finished ]; then
-	#$HOME/bin/synchronize.sh --nodelete UP $S3_UPLOAD_BAM $BAM && touch bam.finished
-fi
-
-if [ ! -e bai.finished ]; then
-	#$HOME/bin/synchronize.sh --nodelete UP $S3_UPLOAD_BAI $BAI && touch bai.finished
-fi
+$HOME/bin/synchronize.sh --nodelete UP $S3_UPLOAD_PATH $BAM
+$HOME/bin/synchronize.sh --nodelete UP $S3_UPLOAD_PATH $BAI
 
 echo "Done."

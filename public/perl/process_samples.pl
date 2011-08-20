5ad3cddf57bd910b88cddae8e5c829e81736faab
gatk-lilly <ngslilly@yahoo.com>
2011-08-20 00:17:35 -0400

    Sample-level dispatcher and job script

---
+++ public/perl/process_samples.pl
@@ -0,0 +1,186 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Data::Dumper;
+use File::Path;
+
+sub usage {
+	my %args = @_;
+
+	my $maxArgLength = 0;
+	foreach my $key (keys(%args)) {
+		if (length($key) > $maxArgLength) {
+			$maxArgLength = length($key);
+		}
+	}
+	my $format = "   %-${maxArgLength}s     [value=%s]\n";
+
+	print "Usage: $0 [--arg1=value1 --arg2=value2 ...]\n\n";
+
+	print "Required arguments:\n";
+	foreach my $arg (sort { $a cmp $b } keys(%args)) {
+		if (!defined($args{$arg})) {
+			print "   --$arg\n";
+		}
+	}
+	print "\n";
+
+	print "Optional arguments:\n";
+	foreach my $arg (sort { $a cmp $b } keys(%args)) {
+		if (defined($args{$arg})) {
+			printf($format, "--$arg", $args{$arg});
+		}
+	}
+	print "\n";
+
+	exit(0);
+}
+
+sub getArgs {
+	my %args = @_;
+
+	if (scalar(@ARGV) == 0 || scalar(grep(/-h|-\?|--help/, @ARGV)) > 0) {
+		&usage(%args);
+	}
+
+	my $args = join(" ", @ARGV);	
+	$args =~ s/=\s+/=/g;
+	my @args = split(/\s+/, $args);
+
+	foreach my $arg (@args) {
+		$arg =~ s/^-+//g;
+		my ($key, $value) = split(/=/, $arg);
+
+		if (exists($args{$key})) {
+			$args{$key} = $value;
+		}
+	}
+
+	my $missingValues = 0;
+	foreach my $arg (keys(%args)) {
+		if (!defined($args{$arg})) {
+			print "Argument '$arg' is required, but has not been specified.\n";
+			$missingValues = 1;
+		}
+	}
+
+	if ($missingValues == 1) {
+		print "\nSome arguments were missing.  Rerun with the -h argument to get usage information.\n";
+		exit(1);
+	}
+
+	return %args;
+}
+
+sub create_condor_submission_file {
+	my ($id, @args) = @_;
+
+	my $jobdir = ".condor_submit/$id";
+
+	my $submission_file = "$jobdir/submit";
+	if (! -e $jobdir) {
+		mkpath($jobdir);
+	}
+
+	open(CSF, ">$submission_file");
+
+	print CSF "Universe = vanilla\n";
+	print CSF "Requirements = (OpSys =?= \"LINUX\") && (SlotID == 1)\n";
+	print CSF "Executable = $ENV{'HOME'}/opt/GATK-Lilly/public/shell/process_one_sample.sh\n";
+	print CSF "Arguments = " . join(" ", @args) . "\n";
+	print CSF "input = /dev/null\n";
+	print CSF "output = $jobdir/log.out\n";
+	print CSF "error = $jobdir/log.err\n";
+	print CSF "notification = Never\n";
+	print CSF "should_transfer_files = YES\n";
+	print CSF "when_to_transfer_output = ON_EXIT\n";
+	print CSF "Queue\n";
+
+	close(CSF);
+
+	return $submission_file;
+}
+
+sub get_s3_contents {
+	my ($s3_path) = @_;
+	my %s3;
+
+	chomp(my @files = qx(s3cmd ls -r $s3_path));
+
+	foreach my $line (@files) {
+		my ($date, $time, $size, $file) = split(/\s+/, $line);
+
+		$s3{$file} = $size;
+	}
+
+	return %s3;
+}
+
+my %args = &getArgs("s3_manifest" => undef, "s3_lane_path" => undef, "s3_upload_path" => undef, "run" => 0);
+
+my ($s3_root) = $args{'s3_upload_path'} =~ /(s3:\/\/.+?\/)/;
+my %s3 = &get_s3_contents($s3_root);
+
+my @entries;
+
+open(MANIFEST, $args{'s3_manifest'});
+chomp(my $header = <MANIFEST>);
+my @header = split(/\s+/, $header);
+
+while (my $line = <MANIFEST>) {
+	next if $line =~ /#/;
+	chomp($line);
+
+	my @entry = split(/\s+/, $line);
+	my %entry;
+
+	for (my $i = 0; $i <= $#entry; $i++) {
+		$entry{$header[$i]} = $entry[$i];
+	}
+
+	push(@entries, \%entry);
+}
+close(MANIFEST);
+
+my %samples;
+foreach my $entry (@entries) {
+	my %entry = %$entry;
+	my $lanebam = "$args{'s3_lane_path'}/$entry{'sample'}/$entry{'flowcell'}.$entry{'lane'}/$entry{'flowcell'}.$entry{'lane'}.bam";
+	push(@{$samples{$entry{'sample'}}}, $lanebam);
+}
+
+foreach my $sample (keys(%samples)) {
+	my @s3lanebams = @{$samples{$sample}};
+	my $s3samplebam = "$args{'s3_upload_path'}/$sample/$sample.bam";
+
+	if (exists($s3{$s3samplebam})) {
+		print "Skipping sample-level pipeline for $sample because the result is already present in S3.\n";
+	} else {
+		my $allLanesPresent = 1;
+		foreach my $s3lanebam (@s3lanebams) {
+			if (!exists($s3{$s3lanebam})) {
+				$allLanesPresent = 0;
+			}
+		}
+
+		if ($allLanesPresent) {
+			my @cmdargs = ($sample, $s3_root, $args{'s3_upload_path'}, @s3lanebams);
+
+			my $cmd = "$ENV{'HOME'}/opt/GATK-Lilly/public/shell/process_one_sample.sh " . join(" ", @cmdargs);
+			print "$cmd\n";
+
+			my $submission_file = &create_condor_submission_file($sample, @cmdargs);
+			my $submission_cmd = "condor_submit $submission_file";
+
+			if ($args{'run'} == 1) {
+				print "Dispatching sample-level pipeline for $sample ($submission_file)\n";
+				system($submission_cmd);
+			} else {
+				#print "Simulating dispatch of sample-level pipeline for $sample ($submission_file)\n";
+				#print "$submission_cmd\n";
+			}
+		} else {
+			print "Skipping sample-level pipeline for $sample because some expected lane BAMs are not present in S3.\n";
+		}
+	}
+}
---
+++ public/shell/process_one_sample.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#$ -N LaneProcessingPipeline
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+#$ -m be
+
+#set -x
+
+export SM=$1
+export S3_BUCKET=`echo $2 | sed 's/s3:\/\///' | sed 's/\/$//'`
+export S3_UPLOAD_ROOT=`echo $3 | sed 's/s3:\/\///' | sed 's/\/$//'`
+export S3_UPLOAD_PATH="$S3_UPLOAD_ROOT/$SM"
+export LANES=${@:4:$#}
+
+export JAVA_HOME=$HOME/opt/jdk1.6.0_27/
+export PATH=$HOME/opt/apache-ant-1.8.2/bootstrap/bin:$HOME/opt/apache-ant-1.8.2/dist/bin:$HOME/opt/jdk1.6.0_27/bin:$HOME/opt/jdk1.6.0_27/db/bin:$HOME/opt/jdk1.6.0_27/jre/bin:$HOME/opt/git-1.7.6/bin:$HOME/opt/git-1.7.6/perl/blib/bin:$HOME/opt/bwa-0.5.9:$HOME/opt/samtools-0.1.17:$HOME/opt/jets3t-0.8.1/bin:$PATH
+export JETS3T_HOME=$HOME/opt/jets3t-0.8.1
+
+export WORK=/mnt/scratch/$SM/aggregation
+export RESOURCES=$WORK/resources
+export TMP=$WORK/tmp
+export DATA=$WORK/data
+export LISTS=$WORK/lists
+
+export GATK="java -Djava.io.tmpdir=$TMP -jar $HOME/opt/GATK-Lilly/dist/GenomeAnalysisTK.jar";
+export QUEUE="java -Djava.io.tmpdir=$TMP -jar $HOME/opt/GATK-Lilly/dist/Queue.jar";
+export CPUS=`cat /proc/cpuinfo | grep -c processor`
+
+export BAM=$SM.bam
+export BAI=$SM.bai
+
+echo "Creating $WORK directory..."
+rm -rf $WORK
+mkdir -p $WORK
+mkdir $DATA
+mkdir $TMP
+mkdir $LISTS
+
+echo "Changing to $WORK directory..."
+cd $WORK
+
+echo "Extracting NGS resources..."
+s3cmd sync s3://$S3_BUCKET/resources $WORK/
+gunzip -c $RESOURCES/resources.tar.gz | tar -C $RESOURCES -xf -
+
+echo "Downloading lane BAMs..."
+for LANE_BAM in $LANES
+do
+	LANE_BAI=`echo $LANE_BAM | sed 's/.bam/.bai/'`
+
+	BAM_BASENAME=`basename $LANE_BAM`
+	BAI_BASENAME=`basename $LANE_BAI`
+
+	s3cmd sync $LANE_BAM $DATA/$BAM_BASENAME
+	s3cmd sync $LANE_BAI $DATA/$BAI_BASENAME
+done
+BAM_LIST="$LISTS/$SM.bam.list"
+find $DATA -name \*.bam > $BAM_LIST
+
+echo "Running sample-level pipeline..."
+$QUEUE -S $HOME/opt/GATK-Lilly/public/scala/qscript/org/broadinstitute/sting/queue/qscripts/DataProcessingPipeline.scala -i $BAM_LIST -r $HOME/opt/GATK-Lilly/public/R/ -R $RESOURCES/ucsc.hg19.fasta -D $RESOURCES/dbsnp_132.hg19.vcf -indels $RESOURCES/1000G_indels_for_realignment.hg19.vcf -project aggregated 
+mv aggregated.$SM.bam $BAM
+mv aggregated.$SM.bai $BAI
+
+echo "Uploading results..."
+$JETS3T_HOME/bin/synchronize.sh --nodelete UP $S3_UPLOAD_PATH $BAM
+$JETS3T_HOME/bin/synchronize.sh --nodelete UP $S3_UPLOAD_PATH $BAI
+
+echo "Done."

9c1d8abe9dccca2654d5f85b92745b5bf99e5622
gatk-lilly <ngslilly@yahoo.com>
2011-10-28 15:59:23 -0400

    new s3 upload replacing jetset

---
+++ public/python/s3_upload.py
@@ -0,0 +1,138 @@
+import boto.s3
+import boto.s3.connection
+from filechunkio import FileChunkIO
+import glob
+import logging
+import math
+import mimetypes
+from multiprocessing import Pool
+import os
+import sys
+from time import strftime
+
+
+def get_conn():
+
+    aws_access_key_id = os.environ["AWS_ACCESS_KEY"]
+    aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+
+    calling_format=boto.s3.connection.OrdinaryCallingFormat()
+    conn = boto.s3.connection.S3Connection(aws_access_key_id=aws_access_key_id,
+                                           aws_secret_access_key=aws_secret_access_key)
+
+    return conn
+
+
+def _upload_part(bucketname, multipart_id, part_num, source_path, offset, bytes, amount_of_retries=10):
+    """
+    Uploads a part with retries.
+    """
+    def _upload(retries_left=amount_of_retries):
+        try:
+            log('Start uploading part #%d ...' % part_num)
+            conn = get_conn()
+            bucket = conn.get_bucket(bucketname)
+            for mp in bucket.get_all_multipart_uploads():
+                if mp.id == multipart_id:
+                    with FileChunkIO(source_path, 'r', offset=offset,
+                        bytes=bytes) as fp:
+                        mp.upload_part_from_file(fp=fp, part_num=part_num)
+                    break
+        except Exception, exc:
+            if retries_left:
+                _upload(retries_left=retries_left - 1)
+            else:
+                log('... Failed uploading part #%d' % part_num)
+                raise exc
+        else:
+            log('... Uploaded part #%d' % part_num)
+
+    _upload()
+
+
+def split_url(url):
+    protocol, host_port = url.split("//")
+    host, port = host_port.split(":")
+    
+    return host, port
+
+
+def upload(bucketname, source_path, keyname, acl='private', headers={}, guess_mimetype=True, parallel_processes=33):
+    """
+    Parallel multipart upload.
+    """
+    conn = get_conn()
+    bucket = conn.get_bucket(bucketname)
+    
+    if guess_mimetype:
+        mtype = mimetypes.guess_type(keyname)[0] or 'application/octet-stream'
+        headers.update({'Content-Type': mtype})
+
+    mp = bucket.initiate_multipart_upload(keyname, headers=headers)
+
+    source_size = os.stat(source_path).st_size
+    bytes_per_chunk = max(int(math.sqrt(5242880) * math.sqrt(source_size)),
+        5242880)
+    chunk_amount = int(math.ceil(source_size / float(bytes_per_chunk)))
+  
+    log("Uploading %s, %s bytes in %s chunks" % (source_path, source_size, chunk_amount))
+
+    pool = Pool(processes=parallel_processes)
+    for i in range(chunk_amount):
+        offset = i * bytes_per_chunk
+        remaining_bytes = source_size - offset
+        bytes = min([bytes_per_chunk, remaining_bytes])
+        part_num = i + 1
+        pool.apply_async(_upload_part, [bucketname, mp.id, part_num, source_path, offset, bytes])
+    pool.close()
+    pool.join()
+
+    if len(mp.get_all_parts()) == chunk_amount:
+        mp.complete_upload()
+        key = bucket.get_key(keyname)
+        key.set_acl(acl)
+    else:
+        mp.cancel_upload()
+
+
+def log(message):
+    print("[%s] - %s" % (strftime("%Y-%m-%d %H:%M:%S"), message))
+    
+
+def get_file_paths(directory, extension):
+    return glob.glob(os.path.join(directory, extension))
+    
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) > 1:
+         log("start")
+
+         script_name = "s3_upload.py"         
+         bucket_name = sys.argv[1]
+         dir_path = sys.argv[2]
+         s3_path = sys.argv[3]
+         ext = sys.argv[4]
+
+         file_paths = get_file_paths(dir_path, "*" + ext)
+         for file_path in file_paths:
+             file_name = os.path.basename(file_path)
+             s3_file_path = s3_path + "/" + file_name
+             upload(bucket_name, file_path, s3_file_path)
+         log("end")   
+    else:
+        help = """
+usage: python s3_upload.py [s3 bucket name] [directory path] [s3_path] [extension]
+
+There are three environment variables that you need you need to have set:
+
+AWS_ACCESS_KEY
+AWS_SECRET_ACCESS_KEY 
+http_proxy
+
+You also must be using a python instance that has the boto package installed: 
+
+export PATH=/lrlhps/apps/python/Python-2.7.2/bin:$PATH
+"""
+        print help
+        
